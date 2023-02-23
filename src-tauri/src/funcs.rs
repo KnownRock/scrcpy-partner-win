45ccf21a66cd9ef2be0ac9495552b3647a2965e8
KnownRock <knownrock@hotmail.com>
2023-02-23 16:46:24 +0800

    fix device vaild logical

--- src-tauri/src/funcs.rs
+++ src-tauri/src/funcs.rs
@@ -4,20 +4,24 @@ pub fn is_device_valid_args(args: Vec<String>) -> bool {
     // let args: Vec<String> = env::args().collect();
 
     let devices_ids = get_adb_devices();
+
+    is_device_valid_args_by_devices(args, devices_ids)
+}
+
+fn is_device_valid_args_by_devices(args: Vec<String>, devices_ids: Vec<String>) -> bool {
     if devices_ids.len() == 0 {
         return false;
     }
 
     let mut have_device_arg_flag = false;
 
-    for arg in &args {
+    for (_, arg) in args.iter().enumerate() {
         for device_id in &devices_ids {
-            if arg == &format!("--serial {}", device_id)
-              || arg == &format!("--serial={}", device_id)
-              || arg == &format!("-s{}", device_id)
-              || arg.starts_with("--tcpip")
-              // TODO: check next -s arg
-              || arg == "-s"
+            if arg == &format!("--serial={}", device_id)
+                || arg == &format!("-s{}", device_id)
+                || arg == &format!("--serial {}", device_id)
+                || arg == &format!("-s {}", device_id)
+                || arg.starts_with("--tcpip")
             {
                 have_device_arg_flag = true;
                 break;
@@ -25,4 +29,63 @@ pub fn is_device_valid_args(args: Vec<String>) -> bool {
         }
     }
     have_device_arg_flag
+}
+
+#[test]
+fn test_is_device_valid_args_by_devices() {
+    assert_eq!(
+        is_device_valid_args_by_devices(
+            vec!["scrcpy.exe".to_string(), "-s1234567890".to_string()],
+            vec!["1234567890".to_string(), "1234567891".to_string()]
+        ),
+        true
+    );
+
+    assert_eq!(
+        is_device_valid_args_by_devices(
+            vec!["scrcpy.exe".to_string(), "--serial=1234567890".to_string()],
+            vec!["1234567891".to_string()]
+        ),
+        false
+    );
+
+    assert_eq!(
+        is_device_valid_args_by_devices(
+            vec!["scrcpy.exe".to_string(), "--serial=1234567890".to_string()],
+            vec!["1234567890".to_string(), "1234567891".to_string()]
+        ),
+        true
+    );
+
+    assert_eq!(
+        is_device_valid_args_by_devices(
+            vec!["scrcpy.exe".to_string(), "--serial 1234567890".to_string()],
+            vec!["1234567890".to_string(), "1234567891".to_string()]
+        ),
+        true
+    );
+
+    assert_eq!(
+        is_device_valid_args_by_devices(
+            vec!["scrcpy.exe".to_string(), "-s 1234567890".to_string()],
+            vec!["1234567890".to_string(), "1234567891".to_string()]
+        ),
+        true
+    );
+
+    assert_eq!(
+        is_device_valid_args_by_devices(
+            vec!["scrcpy.exe".to_string(), "--serial 1234567893".to_string()],
+            vec!["1234567890".to_string(), "1234567891".to_string()]
+        ),
+        false
+    );
+
+    assert_eq!(
+        is_device_valid_args_by_devices(
+            vec!["scrcpy.exe".to_string(), "-s 1234567893".to_string()],
+            vec!["1234567890".to_string(), "1234567891".to_string()]
+        ),
+        false
+    );
 }
