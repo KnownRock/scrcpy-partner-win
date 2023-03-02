6a259f9f0de9a1efb5b95d9ec86b66dcebda5d3b
KnownRock <knownrock@hotmail.com>
2023-03-02 17:24:48 +0800

    add debug mode

--- src-tauri/src/tauri_funcs.rs
+++ src-tauri/src/tauri_funcs.rs
@@ -1,6 +1,7 @@
 use tauri::LogicalSize;
 use tauri::Size;
 
+#[cfg(not(debug_assertions))]
 pub fn init_main_window(app: &tauri::AppHandle) {
     tauri::WindowBuilder::new(app, "main", tauri::WindowUrl::App("index.html".into()))
         .center()
@@ -10,11 +11,42 @@ pub fn init_main_window(app: &tauri::AppHandle) {
         .unwrap();
 }
 
+#[cfg(debug_assertions)]
+pub fn init_main_window(app: &tauri::AppHandle) {
+    tauri::WindowBuilder::new(app, "main", tauri::WindowUrl::App("index.html".into()))
+        .center()
+        // .visible(false)
+        .title("Scrcpy Partner")
+        .build()
+        .unwrap();
+}
+
+#[cfg(not(debug_assertions))]
 pub fn init_tool_window(app: &tauri::AppHandle) {
     let tool_window =
         tauri::WindowBuilder::new(app, "tool", tauri::WindowUrl::App("tool.html".into()))
             // .center()
             .visible(false)
+            .decorations(false)
+            .resizable(false)
+            .skip_taskbar(true)
+            .build()
+            .unwrap();
+
+    tool_window
+        .set_size(Size::Logical(LogicalSize {
+            width: 48.0,
+            height: 600.0,
+        }))
+        .unwrap();
+}
+
+#[cfg(debug_assertions)]
+pub fn init_tool_window(app: &tauri::AppHandle) {
+    let tool_window =
+        tauri::WindowBuilder::new(app, "tool", tauri::WindowUrl::App("tool.html".into()))
+            // .center()
+            // .visible(false)
             .decorations(false)
             .resizable(false)
             .skip_taskbar(true)
