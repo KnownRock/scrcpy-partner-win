1fbb94c8b9ba1ffc8b33e43ae0904c691c04b81c
KnownRock <knownrock@hotmail.com>
2023-03-10 17:12:48 +0800

    refactor types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -10,97 +10,73 @@ interface DialogFormButton {
   action?: string
 }
 
-type FormItem = {
+type FormItem = ({
   type: 'option'
-  label: string
-  name: string
   options: Array<{ label: string, value: string }>
   value: string
   defaultValue?: string
-  disabled?: boolean
 }
 | {
   type: 'message'
-  label: string
-  name: string
   value: string
 }
 | {
   type: 'switch'
-  label: string
-  name: string
   value: boolean
-  disabled?: boolean
 }
 | {
   type: 'text'
-  label: string
-  name: string
   value: string
-  disabled?: boolean
 }
 | {
   type: 'number'
-  label: string
-  name: string
   value: number
-  disabled?: boolean
 }
 | {
   type: 'auto'
-  label: string
-  name: string
   value: string
   options: string[]
-  disabled?: boolean
 }
 | {
   type: 'optional-auto'
-  label: string
-  name: string
   value: string
   enable: boolean
   options: string[]
-  disabled?: boolean
 }
 | {
   type: 'optional-text'
-  label: string
-  name: string
   value: string
   enable: boolean
-  disabled?: boolean
 }
 | {
   type: 'optional-number'
-  label: string
-  name: string
   value: number
   enable: boolean
-  disabled?: boolean
+}
+| {
+  type: 'optional-switch'
+  value: boolean
+  enable: boolean
+}
+| {
+  type: 'optional-auto-number'
+  value: number
+  enable: boolean
+  options: number[]
 }
 | {
   type: 'optional-option'
-  label: string
-  name: string
   options: Array<{ label: string, value: string }>
   value: string
   defaultValue?: string
   enable: boolean
-  disabled?: boolean
 }
 | {
   type: 'header'
-  label: string
-  name: string
-  disabled?: boolean
 }
 | {
   type: 'table'
-  label: string
-  name: string
   value: Array<Record<string, any>>
-  disabled?: boolean
   columns: Array<{
     label: string
     name: string
@@ -110,4 +86,9 @@ type FormItem = {
     }>
   }>
 
+}) & {
+  label: string
+  name: string
+  description?: string
+  disabled?: boolean
 }
