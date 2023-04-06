2ef66f5a115209a1c4483867e520e9f5301bde12
KnownRock <knownrock@hotmail.com>
2023-04-06 15:02:36 +0800

    fix some error output by a hacky way

--- src/splashscreen.ts
+++ src/splashscreen.ts
@@ -1,8 +1,9 @@
 import './style.css'
 import './i18n/index'
 import Loading from './lib/LoadingWithI18n.svelte'
+const Loading2 = Loading as unknown as new (options: { target: HTMLElement }) => Loading
 
-const app = new Loading({
+const app = new Loading2({
   target: document.getElementById('app') as HTMLElement
 })
 
--- src/tool.ts
+++ src/tool.ts
@@ -1,8 +1,9 @@
 // import "./style.css";
 import './i18n/index'
 import App from './lib/ToolWithI18n.svelte'
+const App2 = App as unknown as new (options: { target: HTMLElement }) => App
 
-const app = new App({
+const app = new App2({
   target: document.getElementById('app') as HTMLElement
 })
 
