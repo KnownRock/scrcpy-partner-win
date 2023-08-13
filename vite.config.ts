1f5e58a88c653ac5868004f2419ba19cfc21ca92
KnownRock <KnownRock@hotmail.com>
2023-08-13 16:33:40 +0800

    format

--- vite.config.ts
+++ vite.config.ts
@@ -26,11 +26,11 @@ export default defineConfig({
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
     // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    minify: process.env.TAURI_DEBUG == null ? 'esbuild' : false,
     // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
+    sourcemap: !(process.env.TAURI_DEBUG == null),
 
     rollupOptions: {
       input: {
